580cfaf01bebb978cf6aba218ebaac5e5f0a5e43
ken-1200 <kensuke19971022@gmail.com>
2021-06-22 22:07:46 +0900

    feat: コメント追加

--- todo/cruds/todo.py
+++ todo/cruds/todo.py
@@ -3,23 +3,29 @@ from sqlalchemy.orm import Session
 from todo.models import Todo
 from todo.schemas import TodoCreate, TodoUpdate
 
-
+# id指定
 def get_todo(db: Session, todo_id: int):
     return db.query(Todo).filter(Todo.id == todo_id).first()
 
-
+# 全取得
 def get_todos(db: Session, limit: int = 100):
     return db.query(Todo).limit(limit).all()
 
-
+# 作成
 def create_todo(db: Session, todo: TodoCreate):
     db_todo = Todo(title=todo.title, text=todo.text)
+
+    # データの追加
     db.add(db_todo)
+
+    # データの登録
     db.commit()
+
+    # 一時データのリフレッシュ
     db.refresh(db_todo)
     return db_todo
 
-
+# 更新
 def update_todo(db: Session, todo_id: int, todo: TodoUpdate):
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
     db_todo.title = todo.title
@@ -27,7 +33,7 @@ def update_todo(db: Session, todo_id: int, todo: TodoUpdate):
     db.commit()
     return db_todo
 
-
+# 削除
 def delete_todo(db: Session, todo_id: int):
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
     db.delete(db_todo)
